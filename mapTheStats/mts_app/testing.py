350eed160250084ea25f68d388581196499a6774
andrewingram928 <andrewingram928@gmail.com>
2021-01-05 16:19:10 -0800

    cleaned up EOL errors in MSA_code library, brought in vague census code, tested bea and census api functionality.  Removed settings import, removed msa code import temporarily.

---
+++ mapTheStats/mts_app/testing.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+import requests
+
+## API KEYS
+
+BEA_API_KEY = "692B0866-DAF4-4161-B1FE-A1FBEB3C066F"
+GOOGLE_MAPS_API_KEY = "AIzaSyDaXQ4lvSyI5ERL6GJjOtvGTpxClWGsDOA"
+CENSUS_API_KEY = "51170a508f499d465f5359a5d26af29499a531d3"
+
+## API call for average income = CAINC1
+bea_url = "https://apps.bea.gov/api/data/?UserID={bea_key}&method=GetData&datasetname=Regional&TableName=CAINC1&LineCode=3&Year=2019&GeoFips=STATE&ResultFormat=json".format(
+    bea_key=BEA_API_KEY
+)
+bea_response = requests.get(url=bea_url)
+bea_content = bea_response.json()
+pprint(f"Location Selected: {bea_content['BEAAPI']['Results']['Data'][0]['GeoName']}\nPer Capita Income: {bea_content['BEAAPI']['Results']['Data'][0]['DataValue']}\nYear: {bea_content['BEAAPI']['Results']['Data'][0]['TimePeriod']}")
+
+## API call for percentage of families below poverty level = DP03_0119PE
+census_url = "https://api.census.gov/data/2019/acs/acs1/profile?get=NAME,DP03_0119PE&for=state:06&key={census_key}".format(
+    census_key=CENSUS_API_KEY
+)
+census_response = requests.get(url=census_url)
+census_content = census_response.json()
+pprint(f"Location Selected: {census_content[1][0]}\n\nPercentage of People living below poverty: {census_content[1][1]}")
--- mapTheStats/mts_app/views.py
+++ mapTheStats/mts_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from mapTheStats.settings import GOOGLE_MAPS_API_KEY, BEA_API_KEY
-from local_library.area_codes import *
 from pprint import pprint
 import requests
 
+## API KEYS
+
+BEA_API_KEY = "692B0866-DAF4-4161-B1FE-A1FBEB3C066F"
+GOOGLE_MAPS_API_KEY = "AIzaSyDaXQ4lvSyI5ERL6GJjOtvGTpxClWGsDOA"
+CENSUS_API_KEY = "51170a508f499d465f5359a5d26af29499a531d3"
 
 ## RENDERING
 
@@ -15,11 +18,19 @@ def index (request):
 
 ## API CALLS
 
-def api_call(request):
-    url = "https://apps.bea.gov/api/data/?UserID={api_key}&method=GetData&datasetname=Regional&TableName=CAINC1&LineCode=3&Year=2019&GeoFips=STATE&ResultFormat=json".format(
-    api_key=BEA_API_KEY
+def state_api_call(response):
+    ## BEA API call for average income = CAINC1
+    bea_url = "https://apps.bea.gov/api/data/?UserID={bea_key}&method=GetData&datasetname=Regional&TableName=CAINC1&LineCode=3&Year=2019&GeoFips=STATE&ResultFormat=json".format(
+        bea_key=BEA_API_KEY
     )
-    response = requests.get(url=url)
-    content = response.json()
-    pprint(f"Location Selected: {content['BEAAPI']['Results']['Data'][0]['GeoName']}\nPer Capita Income: {content['BEAAPI']['Results']['Data'][0]['DataValue']}\nYear: {content['BEAAPI']['Results']['Data'][0]['TimePeriod']}")
-    return redirect('/')
+    bea_response = requests.get(url=bea_url)
+    bea_content = bea_response.json()
+    pprint(f"Location Selected: {bea_content['BEAAPI']['Results']['Data'][0]['GeoName']}\nPer Capita Income: {bea_content['BEAAPI']['Results']['Data'][0]['DataValue']}\nYear: {bea_content['BEAAPI']['Results']['Data'][0]['TimePeriod']}")
+
+    ## CENSUS API call for percentage of families below poverty level = DP03_0119PE
+    census_url = "https://api.census.gov/data/2019/acs/acs1/profile?get=NAME,DP03_0119PE&for=state:06&key={census_key}".format(
+    census_key=CENSUS_API_KEY
+    )
+    census_response = requests.get(url=census_url)
+    census_content = census_response.json()
+    pprint(f"Location Selected: {census_content[1][0]}\n\nPercentage of People living below poverty: {census_content[1][1]}")
