5d375be42dac39e60c1b7a1ba6769e25c2ad90c8
dreambender90 <simplexetv90@gmail.com>
2021-01-05 16:09:12 -0800

    map will zoom to state selected

--- mapTheStats/mts_app/map.py
+++
@@ -1,9 +0,0 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-
-fig = plt.figure(figsize=)
-m = Basemap(projection='mill')
-
-m.drawcostlines()
-
-plt.show()
--- mapTheStats/mts_app/views.py
+++ mapTheStats/mts_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from mapTheStats.settings import GOOGLE_MAPS_API_KEY
 
+
 def home (request):
     context = {
         'gmaps' : GOOGLE_MAPS_API_KEY,
